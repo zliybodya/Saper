38df8d52576c2587678c5e02c293bba200648ee0
zliybodya <98189916+zliybodya@users.noreply.github.com>
2026-01-31 15:40:42 +0200

    Update create_venv.bat

--- create_venv.bat
+++ create_venv.bat
@@ -1,29 +1,19 @@
 @echo off
 chcp 65001 > nul
-echo Konfiguracja środowiska projektu
+echo Uruchamianie projektu
 
-set /p choice="Czy chcesz utworzyć środowisko wirtualne (venv) i zainstalować pakiety? (y/n): "
-
-if /i "%choice%"=="y" (
-
-    if not exist venv (
-        echo Tworzenie venv...
-        python -m venv venv
-    ) else (
-        echo venv już istnieje
-    )
-
-    call venv\Scripts\activate
-
-    if exist import_lib.txt (
-        pip install -r import_lib.txt
-    ) else (
-        echo Brak pliku import_lib.txt
-    )
-
-    echo Gotowe!
+if not exist venv (
+    echo Brak srodowiska wirtualnego (venv).
+    echo Najpierw uruchom plik konfiguracyjny.
     pause
-) else (
-    echo Przerwano konfigurację.
-    pause
+    exit /b
 )
+
+echo Aktywacja srodowiska wirtualnego...
+call venv\Scripts\activate
+
+echo Uruchamianie pliku grafic.py...
+python grafic.py
+
+echo Zakonczono dzialanie programu.
+pause
